a60e1ffa73451cc94d6372aa5839c3db4ecd2076
Crazykwak <kwj1830@naver.com>
2022-12-19 12:36:33 +0900

    feat: JWTService 추가 (액세스, 리프레시 토큰 발급, 만료시간 체크 메서드)

--- src/main/java/crazykwak/lotto/jwt/JWTService.java
+++ src/main/java/crazykwak/lotto/jwt/JWTService.java
@@ -1,10 +1,42 @@
 package crazykwak.lotto.jwt;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
 
 @Service
 public class JWTService {
 
+    private final String secretCode = "tmp";
+    private final int accessTokenExpireTime = 6000000;
+    private final int refreshTokenExpireTime = 60000000;
+
+    public String getAccessToken(String email, String name, Long memberId) {
+
+        return JWT.create()
+                .withSubject("AccessToken")
+                .withExpiresAt(new Date(System.currentTimeMillis() + accessTokenExpireTime))
+                .withClaim("email", email)
+                .withClaim("name", name)
+                .withClaim("memberId", memberId)
+                .sign(Algorithm.HMAC512(secretCode));
+    }
+
+    public String getRefreshToken(String email, String name, Long memberId) {
+
+        return JWT.create()
+                .withSubject("RefreshToken")
+                .withExpiresAt(new Date(System.currentTimeMillis() + refreshTokenExpireTime))
+                .sign(Algorithm.HMAC512(secretCode));
+    }
+
+    public boolean isExpired(String token) {
+        DecodedJWT decodedJWT = JWT.decode(token);
+        return decodedJWT.getExpiresAt().before(new Date()) ? true : false;
+    }
 
 
 }
